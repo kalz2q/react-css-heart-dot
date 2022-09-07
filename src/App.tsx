132b24ed7f7e74d3e58e323f48050b36aa21c97c
kalz2q <katsu95i@gmail.com>
2022-09-08 06:18:16 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-css-heart-dot"
+  })
   return (
     <div className="App">
-
+      <i className="heart">heart</i>
     </div>
   );
 }

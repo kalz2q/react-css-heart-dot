e92758654bb6870c0cb2e96d216ec774539d41e6
kalz2q <katsu95i@gmail.com>
2022-11-04 03:43:13 +0900

    heartt

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container-1">
-        <div className="heart"></div>
-        <div className="heart-1"></div>
-        <div className="heart-2"></div>
-      </div>
-      <div className="container">
-        <div className="heart-1"></div>
-        <div className="heart-2"></div>
-        <div className="heart-3"></div>
-      </div>
+      <div className="heart"></div>
     </div>
   );
 }

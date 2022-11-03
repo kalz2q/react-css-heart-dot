af2ea133bae9ff32d78dd172cb2c979e287d1c47
kalz2q <katsu95i@gmail.com>
2022-11-04 06:50:29 +0900

    kkk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+const { useState } = React;
 
 function App() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === true) {
+      setChecked(false);
+    }
+    else { setChecked(true) }
+  }
   return (
     <div className="App">
-      <div className="heart"></div>
+      <form>
+        <label className="like">
+          <input id="kkk" type="checkbox" checked onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}>
+          </input>
+          <div className="heart"></div>
+        </label>
+      </form>
+
     </div>
   );
 }

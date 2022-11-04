0dafcfbc6410bc47079a6d7c952d104ec26a9f9d
kalz2q <katsu95i@gmail.com>
2022-11-04 22:08:16 +0900

    MMM

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-const { useRef } = React;
+// const { useRef } = React;
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="App">
       <label className="like" >
-        <input type="checkbox" ref={inputRef}>
+        {/* <input type="checkbox" ref={inputRef}> */}
+        <input type="checkbox" >
         </input>
         <div className="heart"></div>
       </label>

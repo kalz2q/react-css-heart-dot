04f01cbedc1947d2efa107cb389c7831e52dbf91
kalz2q <katsu95i@gmail.com>
2023-01-19 01:44:07 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import './App.css';
-// bulleseye
+// 左右対称にする
+// 座標計算を単純にする
 
 function App() {
   return (
     <div className="App">
       <div className="box">
-        <div className='robot'>
-          <div className='lefteye'>
-            <div className='iris'></div>
-          </div>
-          <div className='righteye'>
-            <div className='iris'></div>
-          </div>
-        </div>
-      </div>
-      <div className="box4triangle">
-        <div className='triangle'>
-          <div className='lefteye'>
-            <div className='iris'></div>
-          </div>
-          <div className='righteye'>
-            <div className='iris'></div>
-          </div>
-        </div>
-      </div>
-      <div className="box4triangle02">
-        <div className='triangle02'>
-          <div className='lefteye02'>
-            <div className='iris'></div>
-          </div>
-          <div className='righteye'>
-            <div className='iris'></div>
-          </div>
-        </div>
+
+        <div className="circle-1">        <div className="eye"></div></div>
+        <div className="circle-2"></div>
       </div>
     </div>
   );
